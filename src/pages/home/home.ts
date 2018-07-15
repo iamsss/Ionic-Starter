c2b1f9c32342de7ac86495a0514ed9e42fe30d0d
iamsss <saurav9760@gmail.com>
2018-07-15 16:35:44 +0530

    Adding Pop Over

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams ,ModalController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams ,ModalController, PopoverController} from 'ionic-angular';
 
 /**
  * Generated class for the HomePage page.
@@ -16,7 +16,11 @@ import { IonicPage, NavController, NavParams ,ModalController} from 'ionic-angul
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public modalCtrl : ModalController) {
+  constructor(
+    public navCtrl: NavController,
+     public navParams: NavParams,
+     public modalCtrl : ModalController,
+    public popoverCtrl: PopoverController) {
   }
 
   ionViewDidLoad() {
@@ -39,9 +43,16 @@ export class HomePage {
   goToLoadingPage() {
     this.navCtrl.push('LoadingPage');
   }
-  public openModal(){
+   openModal(){
     var data = { message : 'hello world' };
     var modalPage = this.modalCtrl.create('ModalPage',data);
     modalPage.present();
+  }
+
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create('PopoverPage');
+    popover.present({
+      ev: myEvent
+    });
   }
 }
