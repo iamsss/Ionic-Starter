37c3547a6dc303a34415b31362ca6106626c742b
iamsss <saurav9760@gmail.com>
2018-07-15 10:42:20 +0530

    Adding Confirm Alert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -72,4 +72,28 @@ export class HomePage {
     prompt.present();
   }
 
+ 
+  showConfirm() {
+    const confirm = this.alertCtrl.create({
+      title: 'Use this lightsaber?',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+
 }
