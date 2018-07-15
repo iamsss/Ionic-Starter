0073a2a34cb3db0b31bfe7d2f2864dc1b68c2ee0
iamsss <saurav9760@gmail.com>
2018-07-15 11:34:01 +0530

    Ion Button Usage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the HomePage page.
@@ -17,7 +16,7 @@ import { AlertController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
@@ -31,94 +30,10 @@ export class HomePage {
   goToActionSheetPage(){
     this.navCtrl.push('ActionsheetPage');
   }
-
-  showAlert() {
-    const alert = this.alertCtrl.create({
-      title: 'New Friend!',
-      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
-      buttons: ['OK']
-    });
-    alert.present();
+  goToAlertPage() {
+    this.navCtrl.push('AlertPage');
   }
-
-  showPrompt() {
-    const prompt = this.alertCtrl.create({
-      title: 'Login',
-      message: "Enter a your name ?",
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Name'
-        },{
-          name: 'section',
-          placeholder: 'Section'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Save',
-          handler: data => {
-            console.log('Your name is ', data.name);
-          }
-        }
-      ]
-    });
-    prompt.present();
-  }
-
- 
-  showConfirm() {
-    const confirm = this.alertCtrl.create({
-      title: 'Use this lightsaber?',
-      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
-      buttons: [
-        {
-          text: 'Disagree',
-          handler: () => {
-            console.log('Disagree clicked');
-          }
-        },
-        {
-          text: 'Agree',
-          handler: () => {
-            console.log('Agree clicked');
-          }
-        }
-      ]
-    });
-    confirm.present();
-  }
-
-  showRadio() {
-    let alert = this.alertCtrl.create();
-    alert.setTitle('Lightsaber color');
-
-    alert.addInput({
-      type: 'radio',
-      label: 'Blue',
-      value: 'blue',
-      checked: true
-    });
-    alert.addInput({
-      type: 'radio',
-      label: 'Black',
-      value: 'black',
-      checked: false
-    });
-
-    alert.addButton('Cancel');
-    alert.addButton({
-      text: 'OK',
-      handler: data => {
-        console.log('Your radio selected',data)
-      }
-    });
-    alert.present();
+  goToButtonPage() {
+    this.navCtrl.push('ButtonPage');
   }
 }
